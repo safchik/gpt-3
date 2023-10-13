5209eb37077f117458ed5b200a307e0074d01ba0
safchik <safchik9@gmail.com>
2023-10-13 17:09:12 +0100

    basic setup completed

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import features from './features.css'
+
+const Features = () => {
+    return (
+        <div>Features</div>
+    )
+}
+
+export default Features
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import possibility from './possibility.css'
+
+const Possibility = () => {
+    return (
+        <div>Possibility</div>
+    )
+}
+
+export default Possibility
