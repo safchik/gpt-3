a581cd54ade653ee655a57237b400c0ede645fd5
safchik <safchik9@gmail.com>
2023-10-31 10:40:11 +0000

    errors corrected

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,19 +5,19 @@ import { google, slack, atlassian, dropbox, shopify } from './imports'
 const Brand = () => (
     <div className="gpt3__brand section__padding">
         <div>
-            <img src={google} />
+            <img src={google} alt='google' />
         </div>
         <div>
-            <img src={slack} />
+            <img src={slack} alt='slack' />
         </div>
         <div>
-            <img src={atlassian} />
+            <img src={atlassian} alt='atlassian' />
         </div>
         <div>
-            <img src={dropbox} />
+            <img src={dropbox} alt='dropbox' />
         </div>
         <div>
-            <img src={shopify} />
+            <img src={shopify} alt='shopify' />
         </div>
     </div>
 );
